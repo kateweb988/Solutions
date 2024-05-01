8771a34e9012d11117638a05ac73bf06a9bfacbb
kateweb988 <katyavov2805@gmail.com>
2024-05-01 20:05:13 +0300

    last

---
+++ src/js/main.js
@@ -0,0 +1,585 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Scroll
+  $('.go_to').click(function () { // ловим клик по ссылке с классом go_to
+    var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+    if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top - 50 }, 800); // анимируем скроолинг к элементу scroll_el
+    }
+    return false; // выключаем стандартное действие
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  $('.menu li a').click(function (event) {
+    $('.menu-btn').toggleClass('active');
+    $('.menu').toggleClass('active');
+    return false;
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  $(document).ready(function () {
+    $(".youtube-link").grtyoutube({
+      autoPlay: true
+    });
+  });
+
+  (function ($) {
+
+    $.fn.grtyoutube = function (options) {
+
+      return this.each(function () {
+
+        // Get video ID
+        var getvideoid = $(this).attr("youtubeid");
+
+        // Default options
+        var settings = $.extend({
+          videoID: getvideoid,
+          autoPlay: true
+        }, options);
+
+        // Convert some values
+        if (settings.autoPlay === true) { settings.autoPlay = 1 } else { settings.autoPlay = 0 }
+
+        // Initialize on click
+        if (getvideoid) {
+          $(this).on("click", function () {
+            $("body").append('<div class="grtvideo-popup">' +
+              '<div class="grtvideo-popup-content">' +
+              '<span class="grtvideo-popup-close">&times;</span>' +
+              '<iframe class="grtyoutube-iframe" src="https://www.youtube.com/embed/' + settings.videoID + '?rel=0&wmode=transparent&autoplay=' + settings.autoPlay + '&iv_load_policy=3" allowfullscreen frameborder="0"></iframe>' +
+              '</div>' +
+              '</div>');
+          });
+        }
+
+        // Close the box on click or escape
+        $(this).on('click', function (event) {
+          event.preventDefault();
+          $(".grtvideo-popup-close, .grtvideo-popup").click(function () {
+            $(".grtvideo-popup").remove();
+          });
+        });
+
+        $(document).keyup(function (event) {
+          if (event.keyCode == 27) {
+            $(".grtvideo-popup").remove();
+          }
+        });
+      });
+    };
+  }(jQuery));
+});
+document.addEventListener('DOMContentLoaded', function () {
+  $('.calc__btn_one').click(function (event) {
+    $('.calc__one').css('display', 'none');
+    $('.calc__two').fadeIn();
+    return false;
+  });
+  $('.calc__btn_two').click(function (event) {
+    $('.calc__two').css('display', 'none');
+    $('.calc__three').fadeIn();
+    return false;
+  });
+  $('.calc__btn_three').click(function (event) {
+    $('.calc__three').css('display', 'none');
+    $('.calc__four').fadeIn();
+    return false;
+  });
+  $('.calc__btn_four').click(function (event) {
+    $('.calc__four').css('display', 'none');
+    $('.calc__five').fadeIn();
+    return false;
+  });
+
+  $('.calc__two .calc__prev').click(function (event) {
+    $('.calc__two').css('display', 'none');
+    $('.calc__one').fadeIn();
+    return false;
+  });
+
+  $('.calc__three .calc__prev').click(function (event) {
+    $('.calc__three').css('display', 'none');
+    $('.calc__two').fadeIn();
+    return false;
+  });
+
+  $('.calc__four .calc__prev').click(function (event) {
+    $('.calc__four').css('display', 'none');
+    $('.calc__three').fadeIn();
+    return false;
+  });
+
+  $('.calc__five .calc__prev').click(function (event) {
+    $('.calc__five').css('display', 'none');
+    $('.calc__four').fadeIn();
+    return false;
+  });
+
+});
+window.addEventListener("DOMContentLoaded", function () {
+  [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+    var keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, ""),
+        new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+        });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+        function (a) {
+          return "\\d{1," + a.length + "}"
+        }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+      if (event.type == "blur" && this.value.length < 5) this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+
+  });
+
+});
+document.addEventListener("DOMContentLoaded", () => {
+  (function ($) {
+    var elActive = '';
+    $.fn.selectCF = function (options) {
+
+      // option
+      var settings = $.extend({
+        color: "#888888", // color
+        backgroundColor: "#FFFFFF", // background
+        change: function () { }, // event change
+      }, options);
+
+      return this.each(function () {
+
+        var selectParent = $(this);
+        list = [],
+          html = '';
+
+        //parameter CSS
+        var width = $(selectParent).width();
+
+        $(selectParent).hide();
+        if ($(selectParent).children('option').length == 0) { return; }
+        $(selectParent).children('option').each(function () {
+          if ($(this).is(':selected')) { s = 1; title = $(this).text(); } else { s = 0; }
+          list.push({
+            value: $(this).attr('value'),
+            text: $(this).text(),
+            selected: s,
+          })
+        })
+
+        // style
+        var style = " background: " + settings.backgroundColor + "; color: " + settings.color + " ";
+
+        html += "<ul class='selectCF'>";
+        html += "<li>";
+        html += "<span class='arrowCF ion-chevron-right' style='" + style + "'></span>";
+        html += "<span class='titleCF' style='" + style + "; width:" + width + "px'>" + title + "</span>";
+        html += "<span class='searchCF' style='" + style + "; width:" + width + "px'><input style='color:" + settings.color + "' /></span>";
+        html += "<ul>";
+        $.each(list, function (k, v) {
+          s = (v.selected == 1) ? "selected" : "";
+          html += "<li value=" + v.value + " class='" + s + "'>" + v.text + "</li>";
+        })
+        html += "</ul>";
+        html += "</li>";
+        html += "</ul>";
+        $(selectParent).after(html);
+        var customSelect = $(this).next('ul.selectCF'); // add Html
+        var seachEl = $(this).next('ul.selectCF').children('li').children('.searchCF');
+        var seachElOption = $(this).next('ul.selectCF').children('li').children('ul').children('li');
+        var seachElInput = $(this).next('ul.selectCF').children('li').children('.searchCF').children('input');
+
+        // handle active select
+        $(customSelect).unbind('click').bind('click', function (e) {
+          e.stopPropagation();
+          if ($(this).hasClass('onCF')) {
+            elActive = '';
+            $(this).removeClass('onCF');
+            $(this).removeClass('searchActive'); $(seachElInput).val('');
+            $(seachElOption).show();
+          } else {
+            if (elActive != '') {
+              $(elActive).removeClass('onCF');
+              $(elActive).removeClass('searchActive'); $(seachElInput).val('');
+              $(seachElOption).show();
+            }
+            elActive = $(this);
+            $(this).addClass('onCF');
+            $(seachEl).children('input').focus();
+          }
+        })
+
+        // handle choose option
+        var optionSelect = $(customSelect).children('li').children('ul').children('li');
+        $(optionSelect).bind('click', function (e) {
+          var value = $(this).attr('value');
+          if ($(this).hasClass('selected')) {
+            //
+          } else {
+            $(optionSelect).removeClass('selected');
+            $(this).addClass('selected');
+            $(customSelect).children('li').children('.titleCF').html($(this).html());
+            $(selectParent).val(value);
+            settings.change.call(selectParent); // call event change
+          }
+        })
+
+        // handle search 
+        $(seachEl).children('input').bind('keyup', function (e) {
+          var value = $(this).val();
+          if (value) {
+            $(customSelect).addClass('searchActive');
+            $(seachElOption).each(function () {
+              if ($(this).text().search(new RegExp(value, "i")) < 0) {
+                // not item
+                $(this).fadeOut();
+              } else {
+                // have item
+                $(this).fadeIn();
+              }
+            })
+          } else {
+            $(customSelect).removeClass('searchActive');
+            $(seachElOption).fadeIn();
+          }
+        })
+
+      });
+    };
+    $(document).click(function () {
+      if (elActive != '') {
+        $(elActive).removeClass('onCF');
+        $(elActive).removeClass('searchActive');
+      }
+    })
+  }(jQuery));
+
+  $(function () {
+    var event_change = $('#event-change');
+    $(".select").selectCF({
+      change: function () {
+        var value = $(this).val();
+        var text = $(this).children('option:selected').html();
+        console.log(value + ' : ' + text);
+        event_change.html(value + ' : ' + text);
+      }
+    });
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  //popup1
+  let popupBg = document.querySelector('.popup__bg');
+  let popup = document.querySelector('.popup');
+  let openPopupButtons = document.querySelectorAll('.a1');
+  let closePopupButton = document.querySelector('.close-popup');
+
+  openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      popupBg.classList.add('active');
+      popup.classList.add('active');
+    })
+  });
+
+  closePopupButton.addEventListener('click', () => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+      popupBg.classList.remove('active');
+      popup.classList.remove('active');
+    }
+  });
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      //ваша функция закрытия окна
+      popupBg.classList.remove('active');
+      popup.classList.remove('active');
+    }
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  const progressCircle = document.querySelector(".autoplay-progress1 svg");
+  const progressContent = document.querySelector(".autoplay-progress1 span");
+  var swiper = new Swiper(".swiper1", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    centeredSlides: false,
+    loop: true,
+    autoplay: {
+      delay: 8450,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next1",
+      prevEl: ".swiper-button-prev1"
+    },
+    on: {
+      autoplayTimeLeft(s, time, progress) {
+        progressCircle.style.setProperty("--progress", 1 - progress);
+        progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+      }
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 0,
+        loop: true,
+        slidesPerView: 1
+      },
+      576: {
+        spaceBetween: 0,
+        slidesPerView: 1
+      },
+      767: {
+        spaceBetween: 10,
+        slidesPerView: 2
+      },
+      992: {
+        spaceBetween: 20,
+        loop: false,
+        slidesPerView: 3
+      },
+      1200: {
+        spaceBetween: 0,
+        slidesPerView: 3
+      }
+    }
+  });
+  const progressCircle2 = document.querySelector(".autoplay-progress2 svg");
+  const progressContent2 = document.querySelector(".autoplay-progress2 span");
+  var swiper2 = new Swiper(".swiper2", {
+    slidesPerView: 2,
+    spaceBetween: 54,
+    centeredSlides: false,
+    loop: true,
+    autoplay: {
+      delay: 8450,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next2",
+      prevEl: ".swiper-button-prev2"
+    },
+    on: {
+      autoplayTimeLeft(s, time, progress2) {
+        progressCircle2.style.setProperty("--progress2", 1 - progress2);
+        progressContent2.textContent = `${Math.ceil(time / 1000)}s`;
+      }
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 0,
+        loop: true,
+        slidesPerView: 1
+      },
+      576: {
+        spaceBetween: 0,
+        slidesPerView: 1
+      },
+      767: {
+        spaceBetween: 20,
+        slidesPerView: 2
+      },
+      992: {
+        spaceBetween: 20,
+        loop: false,
+        slidesPerView: 3
+      },
+      1200: {
+        spaceBetween: 54,
+        slidesPerView: 2
+      }
+    }
+  });
+  const progressCircle3 = document.querySelector(".autoplay-progress3 svg");
+  const progressContent3 = document.querySelector(".autoplay-progress3 span");
+  var swiper3 = new Swiper(".swiper3", {
+    slidesPerView: 4,
+    spaceBetween: 44,
+    centeredSlides: false,
+    loop: true,
+    autoplay: {
+      delay: 8450,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next3",
+      prevEl: ".swiper-button-prev3"
+    },
+    on: {
+      autoplayTimeLeft(s, time, progress3) {
+        progressCircle3.style.setProperty("--progress3", 1 - progress3);
+        progressContent3.textContent = `${Math.ceil(time / 1000)}s`;
+      }
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 0,
+        loop: true,
+        slidesPerView: 1
+      },
+      576: {
+        spaceBetween: 20,
+        loop: false,
+        slidesPerView: 2
+      },
+      767: {
+        spaceBetween: 20,
+        loop: false,
+        slidesPerView: 2
+      },
+      992: {
+        spaceBetween: 20,
+        loop: false,
+        slidesPerView: 3
+      },
+      1200: {
+        spaceBetween: 44,
+        slidesPerView: 4
+      }
+    }
+  });
+  const progressCircle4 = document.querySelector(".autoplay-progress4 svg");
+  const progressContent4 = document.querySelector(".autoplay-progress4 span");
+  var swiper4 = new Swiper(".swiper4", {
+    slidesPerView: 4,
+    spaceBetween: 70,
+    centeredSlides: false,
+    loop: true,
+    autoplay: {
+      delay: 8450,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next4",
+      prevEl: ".swiper-button-prev4"
+    },
+    on: {
+      autoplayTimeLeft(s, time, progress4) {
+        progressCircle4.style.setProperty("--progress4", 1 - progress4);
+        progressContent4.textContent = `${Math.ceil(time / 1000)}s`;
+      }
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 0,
+        loop: true,
+        slidesPerView: 1
+      },
+      576: {
+        spaceBetween: 0,
+        loop: false,
+        slidesPerView: 1
+      },
+      767: {
+        spaceBetween: 20,
+        loop: false,
+        slidesPerView: 2
+      },
+      992: {
+        spaceBetween: 20,
+        loop: false,
+        slidesPerView: 3
+      },
+      1200: {
+        spaceBetween: 70,
+        slidesPerView: 4
+      }
+    }
+  });
+  const swiper5 = new Swiper('.swiper5', {
+    slidesPerView: 3,
+    spaceBetween: 62,
+    navigation: {
+      nextEl: ".swiper-button-next5",
+      prevEl: ".swiper-button-prev5"
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 0,
+        loop: true,
+        slidesPerView: 1
+      },
+      767: {
+        spaceBetween: 10,
+        slidesPerView: 2
+      },
+      992: {
+        spaceBetween: 20,
+        slidesPerView: 3
+      },
+      1200: {
+        spaceBetween: 62,
+        slidesPerView: 3
+      }
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  let menuBtn = document.querySelector('.menu-btn');
+  let menu = document.querySelector('.menu');
+  menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+});
+// svg
+$(function () {
+  jQuery('img.svg').each(function () {
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function (data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+
+      // Add replaced image's ID to the new SVG
+      if (typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if (typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass + ' replaced-svg');
+      }
+
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+
+      // Check if the viewport is set, else we gonna set it if we can.
+      if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+      }
+
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+
+    }, 'xml');
+
+  });
+});
